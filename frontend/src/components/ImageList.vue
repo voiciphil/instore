0455e18683d23d676226333bdf3afd825ba366eb
Seong-phil Park <voiciphil@gmail.com>
2019-11-15 01:10:11 +0900

    Add loading images and fix server's bugs

--- frontend/src/components/ImageList.vue
+++ frontend/src/components/ImageList.vue
@@ -1,32 +1,48 @@
 <template>
   <div>
-    <v-row dense>
-      <v-col
-        v-for="imgLink in imgLinks"
-        v-bind:key="imgLink.no"
-      >
-        <v-card
-          class="mx-auto"
-          max-width="350"
-          v-on:click="onClick(imgLink.link)"
+    <v-container>
+      <v-row dense>
+        <v-col
+          v-for="imgLink in imgLinks"
+          v-bind:key="imgLink.no"
         >
-          <v-img
-            v-bind:src="imgLink.link"
-            height="350"
+          <v-card
+            class="mx-auto mt-7"
+            width="350"
+            v-on:click="onClick(imgLink.link)"
           >
-          </v-img>
-        </v-card>
-      </v-col>
-    </v-row>
+            <v-img
+              height="350"
+              v-bind:src="imgLink.link"
+            >
+            </v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
+import bus from '../bus'
+
 export default {
   data () {
     return {
       imgLinks: []
     }
+  },
+  created () {
+    bus.$on('init', () => {
+      this.imgLinks = []
+    })
+    bus.$on('go', (link, no) => {
+      this.imgLinks.push({
+        link: link,
+        no: no
+      })
+      this.len = this.imgLinks.length
+    })
   },
   methods: {
     onClick (link) {
--- frontend/src/components/Input.vue
+++ frontend/src/components/Input.vue
@@ -4,6 +4,7 @@
       v-model="address"
       class="mt-5 mb-0 ml-10 mr-10"
       outlined="outlined"
+      clearable
       label="URL"
       hint="인스타그램 게시물의 URL을 입력하세요."
     >
@@ -19,6 +20,8 @@
 </template>
 
 <script>
+import bus from '../bus'
+
 export default {
   data () {
     return {
@@ -28,7 +31,15 @@ export default {
   },
   methods: {
     getPictures () {
-
+      bus.$emit('init')
+      this.$axios.post('http://localhost:3000/image/list', {
+        link: this.address
+      }).then((res) => {
+        console.log(res.data.message)
+        for (let i = 0; i < res.data.imgLinks.length; i++) {
+          bus.$emit('go', res.data.imgLinks[i].imgLink, res.data.imgLinks[i].no)
+        }
+      })
     }
   }
 }
