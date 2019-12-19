0f32f42aef48eb7981685aa68d70395fe8d1c8c2
Seong-phil Park <voiciphil@gmail.com>
2019-12-19 17:22:47 +0900

    Add progress bar and modify layout

--- frontend/src/components/home/Input.vue
+++ frontend/src/components/home/Input.vue
@@ -1,22 +1,25 @@
 <template>
   <div class="body">
     <v-text-field
+      class="ma-5"
       v-model="address"
       v-bind:rules="[rule]"
-      class="mt-5 mb-0 ml-10 mr-10"
-      outlined="outlined"
-      clearable
+      v-on:click:append="getPictures"
+      append-icon="mdi-send"
+      outlined
       label="URL"
       hint="인스타그램 게시물의 URL을 입력하세요."
     >
     </v-text-field>
-    <v-btn
-      v-on:click="getPictures"
-      class="mb-10"
-      rounded
+    <v-progress-linear
+      v-bind:indeterminate="inProgress"
+      v-if="inProgress"
+      striped
+      class="mt-n5"
+      height="10"
+      width="300"
     >
-      {{ btn }}
-    </v-btn>
+    </v-progress-linear>
   </div>
 </template>
 
@@ -28,14 +31,17 @@ export default {
     return {
       btn: 'GO!',
       address: '',
-      valid: true
+      valid: true,
+      inProgress: false
     }
   },
   methods: {
     async getPictures () {
       if (this.address !== '') {
         bus.$emit('init')
+        this.inProgress = true
         const res = await this.$axios.post('/api/image/list', { link: this.address })
+        this.inProgress = false
         if (res.data.message === 'success') {
           this.valid = true
           bus.$emit('go', res.data.imgLinks)
